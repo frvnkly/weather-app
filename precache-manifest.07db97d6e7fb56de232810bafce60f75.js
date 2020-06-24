fdd979c6c569dd5f6c04fa88dbda6bb2d95d40ca
Frank Li <li.26.frank@gmail.com>
2020-06-24 13:41:56 -0400

    Updates

---
+++ precache-manifest.07db97d6e7fb56de232810bafce60f75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c28a64659c4bdff7eaa515e84aca0d3",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "47280ae9ea18d8587f2b",
+    "url": "/weather-app/static/css/main.75df9f48.chunk.css"
+  },
+  {
+    "revision": "8472916bd0a81cb65f36",
+    "url": "/weather-app/static/js/2.6eb8b2af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.6eb8b2af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47280ae9ea18d8587f2b",
+    "url": "/weather-app/static/js/main.bc634d2d.chunk.js"
+  },
+  {
+    "revision": "c93af41ddca6efafe45b",
+    "url": "/weather-app/static/js/runtime-main.d0263612.js"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "/weather-app/static/media/mintbean.cf18b629.png"
+  }
+]);
